8a97944e387ed67eaad80f440f68390eead23b06
Nicolas Castellanos <nicolasfelipe00234@gmail.com>
2025-03-08 23:06:19 -0500

    displayed port on log

--- main.go
+++ main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"carbon_calculator/internal/routes"
+	"fmt"
 	"log"
 	"net/http"
 )
 
+const PORT int = 8080
+
 func main() {
 	routes.SetupCalculatorRoutes()
-	err := http.ListenAndServe(":8080", nil)
+	fmt.Println(fmt.Sprintf("server running in http://localhost:%d", PORT))
+	err := http.ListenAndServe(fmt.Sprintf(":%d", PORT), nil)
 	if err != nil {
 		log.Fatal(err)
 	}
