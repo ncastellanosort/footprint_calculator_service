f47042c59816e78df1b8911d8d260e1778a8e709
Nicolas Castellanos <nicolasfelipe00234@gmail.com>
2025-03-08 19:43:35 -0500

    end calculator

--- main.go
+++ main.go
@@ -3,17 +3,16 @@ package main
 import (
 	"carbon_calculator/config"
 	"carbon_calculator/internal/calc"
+	"fmt"
 )
 
 var test_data config.Answers = config.Answers{
 	Transport: []float32{130, 20, 1, 2},
-	Food:      []float32{130, 20, 1, 2},
-	Energy:    []float32{130, 20, 1, 2},
-	Waste:     []float32{130, 20, 1, 2},
+	Food:      []float32{2, 3, 4, 0},
+	Energy:    []float32{12, 8, 2, 0},
+	Waste:     []float32{2, 1, 1, 3},
 }
 
 func main() {
-
-	calc.Calculator(&test_data)
-
+	fmt.Println(fmt.Sprintf("Huella: %.1f CO2e/semana", calc.Calculator(&test_data)))
 }
