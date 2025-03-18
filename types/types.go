4ad9e9dbe31038fc9e24277da6989f8cec02e606
Nicolas Castellanos <nicolasfelipe00234@gmail.com>
2025-03-18 16:36:01 -0500

    organized code

--- types/types.go
+++ types/types.go
@@ -2,9 +2,41 @@ package types
 
 import "time"
 
+type Answers struct {
+	Transport []float32 `json:"transport"`
+	Food      []float32 `json:"food"`
+	Energy    []float32 `json:"energy"`
+	Waste     []float32 `json:"waste"`
+}
+
+type ArrayData struct {
+	Array []float32
+	Index int
+}
+
+type Data struct {
+	Date      string         `json:"date"`
+	Energy    map[string]int `json:"energy"`
+	Food      map[string]int `json:"food"`
+	Transport map[string]int `json:"transport"`
+	Waste     map[string]int `json:"waste"`
+}
+
 type DataMessage struct {
 	Data   Data    `json:"data"`
 	Result float32 `json:"result"`
+}
+
+var EmisionFactors = struct {
+	TransportEmission []float32
+	FoodEmission      []float32
+	EnergyEmission    []float32
+	WasteEmission     []float32
+}{
+	TransportEmission: []float32{2.31, 0.1, 0.15, 0.11},
+	FoodEmission:      []float32{27, 6.9, 3.2, 2},
+	EnergyEmission:    []float32{0.1, 0.202, 1.51, 0.2},
+	WasteEmission:     []float32{0.45, 0.1, 6, 1.3},
 }
 
 type Transport struct {
@@ -45,36 +77,4 @@ type Waste struct {
 	Total          float32 `gorm:"type:float"`
 	User_id        int
 	Date           time.Time
-}
-
-type Answers struct {
-	Transport []float32 `json:"transport"`
-	Food      []float32 `json:"food"`
-	Energy    []float32 `json:"energy"`
-	Waste     []float32 `json:"waste"`
-}
-
-type ArrayData struct {
-	Array []float32
-	Index int
-}
-
-type Data struct {
-	Date      string         `json:"date"`
-	Energy    map[string]int `json:"energy"`
-	Food      map[string]int `json:"food"`
-	Transport map[string]int `json:"transport"`
-	Waste     map[string]int `json:"waste"`
-}
-
-var EmisionFactors = struct {
-	TransportEmission []float32
-	FoodEmission      []float32
-	EnergyEmission    []float32
-	WasteEmission     []float32
-}{
-	TransportEmission: []float32{2.31, 0.1, 0.15, 0.11},
-	FoodEmission:      []float32{27, 6.9, 3.2, 2},
-	EnergyEmission:    []float32{0.1, 0.202, 1.51, 0.2},
-	WasteEmission:     []float32{0.45, 0.1, 6, 1.3},
 }
